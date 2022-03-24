4ec7bb14525732519cfd35af0c9ddcf4b5c03e18
osamaalschameri <74721369+osamaalschameri@users.noreply.github.com>
2022-03-24 18:59:52 +0100

    name change

--- state.h
+++ state.h
@@ -42,7 +42,6 @@ private:
 	std::map<std::string, int> itemAmount;
 	std::map<std::string, std::vector<std::string>> productRecipeMap;
 	std::map<std::string, std::vector<std::pair<std::string, int>>> itemIngredientMap;
-	std::map<std::string,int>factory_item;
 
 public:
 	
